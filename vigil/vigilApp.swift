c57a0056b79430ca0bead896fd7f84deb4360f97
rushilp7 <rushilpatel386@gmail.com>
2026-04-12 21:03:26 -0400

    setup

---
+++ vigil/vigilApp.swift
@@ -0,0 +1,17 @@
+//
+//  vigilApp.swift
+//  vigil
+//
+//  Created by Rushil Patel on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct vigilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
